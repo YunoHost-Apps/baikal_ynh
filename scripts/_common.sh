70646d56756d238cb754942057eb8d9da0c134c4
Gofannon <17145502+Gofannon@users.noreply.github.com>
2019-02-06 16:18:05 +0100

    add new helper 'is_url_handled_rewrited' + use it

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -13,16 +13,25 @@ is_url_handled() {
   [[ ! ${output[0]} =~ \/yunohost\/sso\/ && ${output[1]} != 404 ]]
 }
 
-is_url_handled_custom() {
-  local output=($(curl -k -s -o /dev/null \
-      -w 'x%{redirect_url} %{http_code}' "$1"))
-  echo "¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤"
-  echo "contenu variable 'output' : $output"
-  echo "¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤"
-  # It's handled if it does not redirect to the SSO nor return 404
-  [[ ! ${output[0]} =~ \/yunohost\/sso\/ && ${output[1]} != 404 ]]
-  #[[ ! ${output[0]} =~ \/yunohost\/sso\/ ]] && [[ ${output[1]} != 404 ]]
-  #[[ ! ${output[0]} =~ \/yunohost\/sso\/ ]]
+is_url_handled_rewrited() {
+    local url="$1"
+    # Try to get the url with curl, and keep the http code and an eventual redirection url.
+    local curl_output="$(curl --insecure --silent --output /dev/null \
+      --write-out '%{http_code};%{redirect_url}' "$url")"
+
+    # Cut the output and keep only the first part to keep the http code
+    local http_code="${curl_output%%;*}"
+    # Do the same thing but keep the second part, the redirection url
+    local redirection="${curl_output#*;}"
+
+    # Return 1 if the url isn't handled.
+    # Return 1 if the url has returned 404
+    if [ "$http_code" = "404" ]; then
+        return 1
+    # Return 1 if the url is redirected to the SSO
+    elif [[ $redirection =~ "/yunohost/sso" ]]; then
+        return 1
+    fi
 }
 
 is_url_handled_rewrited() {
