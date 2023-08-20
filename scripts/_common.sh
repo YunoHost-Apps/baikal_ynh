cb033847650c5395f0fba744cb4304daeb169596
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-08-20 09:29:01 +0200

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,12 +7,12 @@
 #=================================================
 
 # dependencies used by the app
-YNH_PHP_VERSION=8.0
+#REMOVEME? YNH_PHP_VERSION=8.0
 
-php_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-ldap"
+#REMOVEME? php_dependencies="php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-ldap"
 
 # dependencies used by the app (must be on a single line)
-pkg_dependencies="$php_dependencies"
+#REMOVEME? pkg_dependencies="$php_dependencies"
 
 #=================================================
 # PERSONAL HELPERS
